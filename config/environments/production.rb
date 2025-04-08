a872b97b3c157cdfaef53fb96e8e4695111b39e7
sarii0213 <anjing.singka@gmail.com>
2025-04-08 14:51:16 +0900

    :shirt: rubocop -A

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  config.hosts << ENV['RAILS_CONFIG_HOSTS'] if ENV['RAILS_CONFIG_HOSTS']
+  config.hosts << ENV["RAILS_CONFIG_HOSTS"] if ENV["RAILS_CONFIG_HOSTS"]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
